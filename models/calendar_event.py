a8f0602ab776029575a606e2e9f7d2e7d36d0f99
EDallas89 <inmsanz89@gmail.com>
2020-06-30 12:40:24 +0100

    [IMP] Second Line for names

--- models/calendar_event.py
+++ models/calendar_event.py
@@ -12,15 +12,22 @@ class CalendarEvent(models.Model):
     partner_name = fields.Char(
         compute='compute_partner_name',
     )
+    attendees_string = fields.Char(
+        compute='compute_partner_name',
+    )
 
     @api.one
-    @api.depends('partner_ids', 'partner_name')
+    @api.depends('partner_ids')
     def compute_partner_name(self):
-        self.partner_name = 'Asistentes: '
+        self.attendees_string = 'Asistentes: '
+        self.partner_name = ''
         total_partner = 1
         separator = ', '
         for partner in self.partner_ids:
             if total_partner == len(self.partner_ids):
                 separator = ''
-            self.partner_name += partner.name + separator
+            if total_partner == 1:
+                self.attendees_string += partner.name + separator
+            else:
+                self.partner_name += partner.name + separator
             total_partner += 1
